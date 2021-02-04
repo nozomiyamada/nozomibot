b174eb78b7984c2d3b3d15cbca62558356042093
Nozomi Yamada <44984892+nozomiyamada@users.noreply.github.com>
2021-02-05 02:09:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -174,7 +174,7 @@ def log_web(mode, text):
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
-@app.route("/callback", methods=['POST'])
+@app.route("/line/callback", methods=['POST'])
 def callback():
 	# get X-Line-Signature header value
 	signature = request.headers['X-Line-Signature']
